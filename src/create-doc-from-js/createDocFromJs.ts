b029a0312922b569c4a43f33dabe930974c22df6
William Alvarez <walvarez1@my.bcit.ca>
2023-07-17 17:11:32 -0700

    chore: initialize project

---
+++ src/create-doc-from-js/createDocFromJs.ts
@@ -0,0 +1,8 @@
+/**
+ * Script to create .md documentation from javascript and typescript code
+ */
+const CreateDocFromJS = async () => {
+   console.log("this script is running");
+};
+
+CreateDocFromJS();
