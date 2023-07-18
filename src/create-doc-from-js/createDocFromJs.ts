f038282fb5342408bf399e6141998812c0253d20
William Alvarez <walvarez1@my.bcit.ca>
2023-07-17 17:18:41 -0700

    chore: basic architecture

--- src/create-doc-from-js/createDocFromJs.ts
+++ src/create-doc-from-js/createDocFromJs.ts
@@ -1,5 +1,7 @@
+import { FILE_EXTENSION } from "./config";
+
 /**
- * Script to create .md documentation from javascript and typescript code
+ * Script to create documentation from javascript and typescript code
  */
 const CreateDocFromJS = async () => {
    console.log("this script is running");
